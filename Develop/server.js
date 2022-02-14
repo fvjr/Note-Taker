aa24af5dc67f03fcd0d7c7f024b26dce8ebe398a
Fernando Vasquez <vasquezjrf@gmail.com>
2022-02-14 17:53:18 -0500

    added initial framework for express, added wildcare route, added port listener

---
+++ Develop/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+//wildcard route to direct users to main html page
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+
+app.listen(PORT, () =>
+console.log(`App listening at http://localhost:${PORT}`)
+)
+
+
+
+
+
