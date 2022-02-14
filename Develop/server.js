549df309910aaaafa8e3e12cf3e131b86ca27fda
Fernando Vasquez <vasquezjrf@gmail.com>
2022-02-14 18:14:50 -0500

    updated routes and requirements in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,19 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs')
+const notes = require('./db/db.json')
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 //route to the notes.html file
 app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+res.json(notes)
 );
 
 //wildcard route to direct users to main html page
@@ -15,6 +23,7 @@ app.get('*', (req, res) =>
 );
 
 
+//port listener
 app.listen(PORT, () =>
 console.log(`App listening at http://localhost:${PORT}`)
 )
