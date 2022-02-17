2972a887980b7a2a15038bb4a8f37bec796517ff
Fernando Vasquez <vasquezjrf@gmail.com>
2022-02-17 17:33:30 -0500

    added route for notes.js

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,7 +1,14 @@
-const notes = require('express').Router();
+const notesRoute = require('express').Router();
+const notes = require('../db/db.json')
+const fs = require('fs');
+
+// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+notesRoute.get('/', (req, res) => {
+  console.info(`${req.method} request received to get notes`);
+  return res.json(notes)
+}
+);
+//
 
 
-
-
-
-module.exports = notes;
+module.exports = notesRoute;
--- Develop/server.js
+++ Develop/server.js
@@ -1,30 +1,54 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-
-//
-//import api
-const api = require('./routes/notes.js')
+//get notes --MOVE TO ROUTES
+const api = require('./routes/index.js');
 
 //middleware
 app.use(express.static('public'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api)
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+
+//get notes --MOVE THIS TO ROUTES
+// app.get('/api/notes', (req, res) =>
+//   res.json(notes)
+// );
+
+// returning json of notes
+// app.get('/api/notes', (req, res) => {
+//   console.info(`${req.method} request received to get notes`);
+//   return res.json(notes)
+// }
+// );
+
 
 
 //
 
-//route to notes html
-app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/notes.html')))
 
-//wild card route to redirect to index html
+
+//html route to default index html
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//html route to notes html
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+//wild card route to direct users to primary index
 app.get('*', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/index.html')))
+  res.sendFile(path.join(__dirname, '/public/index.html')
+  )
+);
 
 //port listener
 app.listen(PORT, () =>
-console.log(`App listening at http://localhost:${PORT}`))
+  console.log(`App listening at http://localhost:${PORT}`)
+);
