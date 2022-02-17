2afa7fe8431718d0e2a50c89a57fc72425cdbdb9
Fernando Vasquez <vasquezjrf@gmail.com>
2022-02-17 17:39:07 -0500

    removed unneeded code from server.js

--- Develop/server.js
+++ Develop/server.js
@@ -3,34 +3,13 @@ const path = require('path');
 const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 const app = express();
-//get notes --MOVE TO ROUTES
 const api = require('./routes/index.js');
 
 //middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
-
 app.use(express.static('public'));
-
-
-//get notes --MOVE THIS TO ROUTES
-// app.get('/api/notes', (req, res) =>
-//   res.json(notes)
-// );
-
-// returning json of notes
-// app.get('/api/notes', (req, res) => {
-//   console.info(`${req.method} request received to get notes`);
-//   return res.json(notes)
-// }
-// );
-
-
-
-//
-
-
 
 //html route to default index html
 app.get('/', (req, res) =>
