a47ffc8fe886e64587a5188bd6f650ea045e8bef
Fernando Vasquez <vasquezjrf@gmail.com>
2022-02-17 11:59:48 -0500

    redid file structure, removed logic for getting api/notes, made routes directory

--- Develop/server.js
+++ Develop/server.js
@@ -2,82 +2,17 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const fs = require('fs')
-const notes = require('./db/db.json')
-//set unique IDs
-const { v4: uuidv4 } = require('uuid');
 
-console.log(uuidv4())
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-//route to the notes.html file
+//route to notes html
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
-);
+res.sendFile(path.join(__dirname, '/public/notes.html')))
 
-//route to return all saved notes as JSON
-app.get('/api/notes', (req, res) =>
-  res.json(notes)
-);
+//wild card route to redirect to index html
+app.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/index.html')))
 
-//route to post notes to db.json file
-app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request received to add a note.`)
-
-  const { title, text} = req.body;
-
-  if (title && text) {
-    const newNote = {
-      title,
-      text,
-      id: uuidv4()
-    };
-
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const parsedNotes = JSON.parse(data);
-
-        parsedNotes.push(newNote);
-
-        fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully added note!')
-        );
-      }
-    });
-
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-
-console.log(response);
-res.status(201).json(response);
-  } else {
-    res.status(500).json('Error in positing note');
-  }
-  });
-
-
-  //wildcard route to direct users to main html page
-  app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-  );
-
-
-  //port listener
-  app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT}`)
-  )
-
-
-
-
-
+//port listener
+app.listen(PORT, () =>
+console.log(`App listening at http://localhost:${PORT}`))
